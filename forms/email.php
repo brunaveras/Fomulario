bc728cfad47523d3c670b3a9aa56b036fb34ff06
Bruna Veras <85624987+brunaveras@users.noreply.github.com>
2022-01-01 09:24:44 -0300

    acréscimos

---
+++ forms/email.php
@@ -0,0 +1,37 @@
+<?php
+
+
+if(isset($_POST['email'] && !empty($_POST['email'])) {
+
+$nome = addslashes($_POST['nome']);
+$telefone = addslashes($_POST['telefone']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['mensagem']);
+
+$to = "verasbruna53@gmail.com";
+$subject = "Contato";
+$body = "Nome: "
+
+
+$to = "verasbruna53@gmail.com";
+$subject = "Solicitação";
+$body = "Nome: ".$nome. "\r\n".
+        "Telefone: ".$telefone. "\r\n".
+        "E-mail: ".$email. "\r\n".
+        "Mensagem: ".$mensagem;
+$header = "From: brunaingri@live.com" . "\r\n".
+        ."Reply-To:".$email."\r\n".
+        ."X-Mailer:PHP/".phpversion();
+        
+if(mail($to,$subject,$body,$header)){
+
+    echo("E-mail enviado com sucesso!")
+
+}else{
+    echo("O e-mail não pode ser enviado.");
+}
+
+
+}
+
+?>
